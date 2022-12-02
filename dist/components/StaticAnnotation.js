40f8276c842c0223555db01d54ba85f032ae6e94
joe <joe.saperstein@alteryx.com>
2022-12-02 16:09:12 -0700

    type pos

--- dist/components/StaticAnnotation.js
+++ dist/components/StaticAnnotation.js
@@ -5,12 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
+const ui_1 = require("@alteryx/ui");
 function StaticAnnotation({ height, width, top, left, onClick, options, type, }) {
     console.log('STATIC ANNO JSAP FORK 1212');
     const styles = options.annoStyles || {};
-    return (react_1.default.createElement("div", null,
-        react_1.default.createElement("p", null, type),
-        react_1.default.createElement("div", { className: "staticAnno", "data-testid": "static-annotation", onClick: onClick, onPointerDown: (e) => e.stopPropagation(), style: Object.assign(Object.assign({}, styles), { height, width, top, left }) })));
+    return (react_1.default.createElement("div", { className: "staticAnno", "data-testid": "static-annotation", onClick: onClick, onPointerDown: (e) => e.stopPropagation(), style: Object.assign(Object.assign({}, styles), { height, width, top, left }) },
+        react_1.default.createElement(ui_1.Typography, { variant: "body1", style: { margin: 0,
+                position: 'absolute',
+                top: '-17px',
+                left: '-50%' } }, type)));
 }
 exports.default = StaticAnnotation;
 //# sourceMappingURL=StaticAnnotation.js.map
--- src/components/StaticAnnotation.tsx
+++ src/components/StaticAnnotation.tsx
@@ -1,7 +1,7 @@
 // Copyright (c) 2022 Alteryx, Inc. All rights reserved.
 
 import React from 'react';
-
+import { Typography } from '@alteryx/ui';
 import { TOptions } from '../types';
 
 export type Props = {
@@ -26,15 +26,17 @@ function StaticAnnotation({
   console.log('STATIC ANNO JSAP FORK 1212')
   const styles = options.annoStyles || {};
   return (
-    <div>
-      <p>{type}</p>
       <div
       className="staticAnno"
       data-testid="static-annotation"
       onClick={onClick}
       onPointerDown={(e) => e.stopPropagation()}
       style={{ ...styles, height, width, top, left }}
-      />
+      >
+        <Typography variant="body1" style={{margin: 0,
+position: 'absolute',
+top: '-17px',
+left: '-50%'}}>{type}</Typography>
     </div>
   );
 }
