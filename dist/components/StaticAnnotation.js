a090b256f39062d74dde056287b4944450b66c6b
joe <joe.saperstein@alteryx.com>
2022-12-05 14:21:34 -0700

    colors in 4

--- dist/components/StaticAnnotation.js
+++ dist/components/StaticAnnotation.js
@@ -7,14 +7,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const ui_1 = require("@alteryx/ui");
 const annoBgStyles = {
-    account_number: 'rgba(155, 59, 136, 0.4)',
-    contact_email: 'rgba(226, 95, 69, 0.4)',
-    due_date: 'rgba(150, 177, 73, 0.4)',
-    invoice_date: 'rgba(76, 114, 182, 0.4)',
-    invoice_number: 'rgba(0, 94, 70, 0.4)',
-    tax_amount: 'rgba(168, 37, 85, 0.4)',
-    total_due: 'rgba(16, 197, 173, 0.4)',
-    vendor_name: 'rgba(255, 112, 130, 0.4)'
+    account_number: 'rgba(155, 59, 136, 0.46)',
+    contact_email: 'rgba(226, 95, 69, 0.46)',
+    due_date: 'rgba(150, 177, 73, 0.46)',
+    invoice_date: 'rgba(76, 114, 182, 0.46)',
+    invoice_number: 'rgba(0, 94, 70, 0.46)',
+    tax_amount: 'rgba(168, 37, 85, 0.46)',
+    total_due: 'rgba(16, 197, 173, 0.46)',
+    vendor_name: 'rgba(255, 112, 130, 0.46)'
 };
 const textStyles = {
     account_number: 'rgba(155, 59, 136, 1)',
@@ -28,7 +28,6 @@ const textStyles = {
 };
 function StaticAnnotation({ height, width, top, left, onClick, options, type, }) {
     const styles = options.annoStyles || {};
-    console.log(height, width);
     return (react_1.default.createElement(react_1.default.Fragment, null, type !== 'other' ? (react_1.default.createElement("div", { className: "staticAnno", "data-testid": "static-annotation", onClick: onClick, onPointerDown: (e) => e.stopPropagation(), style: Object.assign(Object.assign({}, styles), { height, width, top, left, backgroundColor: `${annoBgStyles[type]}` }) },
         react_1.default.createElement(ui_1.Typography, { variant: "body1", style: {
                 margin: 0,
