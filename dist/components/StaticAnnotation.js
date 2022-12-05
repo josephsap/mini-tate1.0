b008fb54417a2161b8c6db64d58622698ff23775
joe <joe.saperstein@alteryx.com>
2022-12-05 13:25:58 -0700

    yo

--- dist/components/StaticAnnotation.js
+++ dist/components/StaticAnnotation.js
@@ -7,13 +7,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
 const ui_1 = require("@alteryx/ui");
 function StaticAnnotation({ height, width, top, left, onClick, options, type, }) {
-    console.log('STATIC ANNO JSAP FORK 1212');
+    console.log(type, 'afjasdkljfkla');
     const styles = options.annoStyles || {};
     return (react_1.default.createElement("div", { className: "staticAnno", "data-testid": "static-annotation", onClick: onClick, onPointerDown: (e) => e.stopPropagation(), style: Object.assign(Object.assign({}, styles), { height, width, top, left }) },
-        react_1.default.createElement(ui_1.Typography, { variant: "body1", style: { margin: 0,
+        react_1.default.createElement(ui_1.Typography, { variant: "body1", style: {
+                margin: 0,
                 position: 'absolute',
                 top: '-17px',
-                left: '-50%' } }, type)));
+                left: '-50%'
+            } }, type)));
 }
 exports.default = StaticAnnotation;
 //# sourceMappingURL=StaticAnnotation.js.map
